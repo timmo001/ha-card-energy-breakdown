c578860a40b0d4820429c2e23e1acf75dad21599
Aidan Timson <aidan@timmo.dev>
2026-04-27 14:53:34 +0100

    Cleanup

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -1,19 +1,5 @@
-import { createRequire } from "node:module";
 import { defineConfig } from "rolldown";
 import serve from "rollup-plugin-serve";
-import ignore from "./build-plugins/ignore-plugin.mjs";
-
-const require = createRequire(import.meta.url);
-
-const IGNORED_FILES = [
-  "@material/mwc-notched-outline/mwc-notched-outline.js",
-  "@material/mwc-ripple/mwc-ripple.js",
-  "@material/mwc-list/mwc-list.js",
-  "@material/mwc-list/mwc-list-item.js",
-  "@material/mwc-menu/mwc-menu.js",
-  "@material/mwc-menu/mwc-menu-surface.js",
-  "@material/mwc-icon/mwc-icon.js",
-];
 
 const dev = process.env.ROLLDOWN_WATCH || process.env.ROLLUP_WATCH;
 
@@ -45,12 +31,7 @@ export default defineConfig({
   transform: {
     target: "es2017",
   },
-  plugins: [
-    ignore({
-      files: IGNORED_FILES.map((file) => require.resolve(file)),
-    }),
-    ...(dev ? [serve(serveOptions)] : []),
-  ],
+  plugins: [...(dev ? [serve(serveOptions)] : [])],
   moduleContext: (id) => {
     if (thisAsWindowForModules.some((id_) => id.trimEnd().endsWith(id_))) {
       return "window";
